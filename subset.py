c14b73e428bf9f0648cc9b02805c9d3fbfdb7de5
ameliameyer <52171886+ameliameyer@users.noreply.github.com>
2021-03-15 12:46:20 -0700

    Update subset.py
    I was using a while loop at first to stop at a specific line but I realized that would give me the same id over and over again. This python code works properly

--- subset.py
+++ subset.py
@@ -8,15 +8,16 @@ nh_sub_ids.txt for rehydration
 
 # open both files
 with open('nh_ids.txt', 'r') as firstfile, open('nh_sub_ids.txt', 'a') as secondfile:
-    # set the variable x as the number of tweet ids you would like to transfer
-    x = 20000
-    # use a for loop to get every line in the file
-    for line in firstfile:
-        # use a while loop to limit the number of lines 
-        while x != 0:
-            # append content to second file
-            secondfile.write(line)
-            # update count variable
-            x -= 1
+        # set i as the number of tweet ids you would like to transfer
+        for i, line in enumerate(firstfile):
+                if i == 20000:
+                        # if the number of lines is 20000, break
+                        break
+                else:
+                        # if it isn't keep adding
+                        secondfile.write(line)
+                
 
+firstfile.close()
+secondfile.close()
 
