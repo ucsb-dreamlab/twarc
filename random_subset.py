6be342d5628cb6803cd56a4f0219221b3d26cf18
ameliameyer <52171886+ameliameyer@users.noreply.github.com>
2021-04-14 13:13:34 -0700

    Update random_subset.py

--- random_subset.py
+++ random_subset.py
@@ -17,9 +17,6 @@ while num_lines < length: # while the number of lines is less than the subset le
         rand_lines.append(num) # add it
         num_lines += 1 # increase the counter
 
-print(len(rand_lines)==length) # check the length of the list is equal to the subset
-
-
 '''  Reading Files; Writing Random Tweet Ids To New File  '''
 with open(tweets, 'r') as firstfile, open(subset, 'a') as secondfile: # open both files
         count = 0 # number of lines in interation
@@ -31,7 +28,7 @@ with open(tweets, 'r') as firstfile, open(subset, 'a') as secondfile: # open bot
                 else: # if it isn't, add it
                         secondfile.write(line) # writing to the file
                         count += 1 # increasing the counter
-                        print(count)
+                        print("Number of tweet ids loaded... ", count)
 
 '''  Close Both Files  '''
 firstfile.close()
