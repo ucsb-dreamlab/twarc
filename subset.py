4f77ab4f445bf26295b27b1e6ab76f5cbeaeee60
ameliameyer <52171886+ameliameyer@users.noreply.github.com>
2021-04-20 12:05:11 -0700

    Update subset.py
    Added in more documentation for usage and download

--- subset.py
+++ subset.py
@@ -1,4 +1,9 @@
 '''
+This python program will take a file of dehydrated tweets and return the first n tweets where n is the user's chosen subset length. To use this program, download it from Github and store it in your twarc utilities file. 
+
+Usage: python utils/subset.py 
+
+
 Takes user input of the following:
 
 tweets - The name of the original tweet id file you'd like to take a subset of. This should be entered without quotes. Example user input: nh_ids.txt
@@ -8,6 +13,7 @@ length - The length you'd like your subset to be. Example user input: 20000
 Each id must be on a separate line in the original tweet file for this to work. The program reads line by line. 
 '''
 
+# getting user input 
 tweets = input("Enter the name of your tweet ids file: ")
 subset = input("Enter the name of the output tweet ids file: ")
 length = int(input("Enter the number of tweets you would like to subset: "))
@@ -23,6 +29,7 @@ with open(tweets, 'r') as firstfile, open(subset, 'a') as secondfile:
                         # if it isn't keep adding
                         secondfile.write(line)
 
+# close both files
 firstfile.close()
 secondfile.close()
 
